aba153b166605a07cdd1f863daf07d64846f4f72
sergio-rosendo-code <sergio.rosendo.code@protonmail.com>
2023-10-12 08:41:37 -0700

    Bash installation scripts to be used in Dockerfile

---
+++ Docker/docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Source: https://docs.docker.com/engine/install/debian/
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
