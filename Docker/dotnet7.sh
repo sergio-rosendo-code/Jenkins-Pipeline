df8f6ccb18d180e020d0416965cc75d9dd248dfc
sergio-rosendo-code <144377912+sergio-rosendo-code@users.noreply.github.com>
2023-10-12 18:49:09 -0700

    Update dotnet7.sh

--- Docker/dotnet7.sh
+++ Docker/dotnet7.sh
@@ -5,5 +5,4 @@ wget https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.de
 rm packages-microsoft-prod.deb
 
-apt-get update
-apt-get install -y dotnet-sdk-7.0.12
+apt-get update && apt-get install -y dotnet-sdk-7.0
